53025a2d9726efc2af163d549a041c098b8e762c
trumpocalyspe <mark.jn.kim@gmail.com>
2017-10-11 13:50:22 -0700

    adjust side navbar, keep on large

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require_tree .
 $( document ).ready(function() {
         $(".button-collapse").sideNav({
-          menuWidth: 200, // Default is 240
+          menuWidth: 230, // Default is 240
           edge: 'left', // Choose the horizontal origin
           closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
           }
