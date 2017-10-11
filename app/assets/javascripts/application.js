698767bb6db59af94515b3679b3d84466edb7ae6
trumpocalyspe <mark.jn.kim@gmail.com>
2017-10-10 17:11:35 -0700

    side nav bar decrease width  and closes on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,12 @@
 
 //= require_tree .
 $( document ).ready(function() {
-        $(".button-collapse").sideNav();
+        $(".button-collapse").sideNav({
+          menuWidth: 200, // Default is 240
+          edge: 'left', // Choose the horizontal origin
+          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          }
+        );
         // $('.button-collapse').sideNav('show');
         // $('.button-collapse').sideNav('hide');
     });
